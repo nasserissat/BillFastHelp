6090a919c3743dd13261f36ade230f379bc89e87
Nasser Emil Issa Tavares <nissa@transnegrd.com>
2022-09-15 16:59:27 -0400

    search bar hcho y side nav en construccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BillFastHelp';
+   multiply(a: number, b: number): number{
+    return a + b;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './Components/searchbar/searchbar.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,

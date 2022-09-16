f1c4aca9dec9678d773155c12fab495cf26dd49c
Nasser Emil Issa Tavares <nissa@transnegrd.com>
2022-09-16 12:08:37 -0400

    sidenav comentado y agregue la propiedad url a la iteracion de los submenus

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -8,40 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class SideNavComponent implements OnInit {
 
   Transacciones = {
-    Facturas: {name:'Facturas', icon: 'fa-solid fa-file-invoice ml-0.5'},
-    Pagos: {name: 'Pagos', icon: 'fa-solid fa-receipt'},
-    CambioDeDivisas:{name: 'Cambios de divisas', icon: 'fa-solid fa-file-zipper'},
-    NotasDeCredito:{name: 'Notas de crédito', icon: 'fa-solid fa-money-check-dollar'},
-    NotasDeDebito:{name: 'Notas de débito', icon: 'fa-solid fa-file-invoice-dollar'},
-    Cargos:{name: 'Cargos', icon: 'fa-solid fa-file-invoice-dollar'},
-    LotesDeFacturacion:{name: 'Lotes de facturación', icon: 'fa-solid fa-file-zipper'},
-    ImpresionMasiva:{name: 'Impresión masiva', icon: 'fa-solid fa-file-zipper'},
+    Facturas: {name:'Facturas', icon: 'fa-solid fa-file-invoice ml-0.5', url: ''},
+    Pagos: {name: 'Pagos', icon: 'fa-solid fa-receipt', url: ''},
+    CambioDeDivisas:{name: 'Cambios de divisas', icon: 'fa-solid fa-file-zipper', url: ''},
+    NotasDeCredito:{name: 'Notas de crédito', icon: 'fa-solid fa-money-check-dollar', url: ''},
+    NotasDeDebito:{name: 'Notas de débito', icon: 'fa-solid fa-file-invoice-dollar', url: ''},
+    Cargos:{name: 'Cargos', icon: 'fa-solid fa-file-invoice-dollar', url: ''},
+    LotesDeFacturacion:{name: 'Lotes de facturación', icon: 'fa-solid fa-file-zipper', url: ''},
+    ImpresionMasiva:{name: 'Impresión masiva', icon: 'fa-solid fa-file-zipper', url: ''},
   }
 
   Catalogos = {
-    Clientes: {name:'Clientes', icon: 'fa-solid fa-id-badge'},
-    Direcciones: {name: 'Direcciones', icon: 'fa-solid fa-signs-post'},
-    Suscripciones:{name: 'Suscripciones', icon: 'fa-solid fa-scroll'},
-    Productos:{name: 'Productos', icon: 'fa-solid fa-boxes-stacked'},
-    Materiales:{name: 'Materiales', icon: 'fa-solid fa-boxes-stacked'},
+    Clientes: {name:'Clientes', icon: 'fa-solid fa-id-badge', url: ''},
+    Direcciones: {name: 'Direcciones', icon: 'fa-solid fa-signs-post', url: ''},
+    Suscripciones:{name: 'Suscripciones', icon: 'fa-solid fa-scroll', url: ''},
+    Productos:{name: 'Productos', icon: 'fa-solid fa-boxes-stacked', url: ''},
+    Materiales:{name: 'Materiales', icon: 'fa-solid fa-boxes-stacked', url: ''},
   }
 
   ServicioAlCliente = {
-    Tickets: {name:'Tickets', icon: 'fa-solid fa-ticket'},
-    Reclamaciones: {name: 'Reclamaciones', icon: 'fa-solid fa-id-badge'},
-    AcuerdosDePago:{name: 'Acuerdos de Pago', icon: 'fa-solid fa-file-signature'},
-    CertificadosDeNoAdeudo:{name: 'Certificados de No Adeudo', icon: 'fa-solid fa-stamp'},
+    Tickets: {name:'Tickets', icon: 'fa-solid fa-ticket', url: ''},
+    Reclamaciones: {name: 'Reclamaciones', icon: 'fa-solid fa-id-badge', url: ''},
+    AcuerdosDePago:{name: 'Acuerdos de Pago', icon: 'fa-solid fa-file-signature', url: ''},
+    CertificadosDeNoAdeudo:{name: 'Certificados de No Adeudo', icon: 'fa-solid fa-stamp', url: ''},
   }
 
   GestionDeTrabajo = {
-    OrdenesDeTrabajo: {name: 'Ordenes de Trabajo', icon: 'fa-solid fa-folder'},
-    TiposDeOrdenesDeTrabajo: {name: 'Tipos de Ordenes de Trabajo', icon: 'fa-solid fa-folder-tree'},
+    OrdenesDeTrabajo: {name: 'Ordenes de Trabajo', icon: 'fa-solid fa-folder', url: ''},
+    TiposDeOrdenesDeTrabajo: {name: 'Tipos de Ordenes de Trabajo', icon: 'fa-solid fa-folder-tree', url: ''},
   }
   Planificacion = {
-    Rutas: {name: 'Rutas', icon: 'fa-solid fa-signs-post'},
-    TiposDeRutas : {name: 'Tipos de rutas', icon: 'fa-solid fa-sitemap'},
-    CiclosDeFacturacion: {name: 'Ciclos de facturación', icon: 'fa-solid fa-arrows-spin'},
-    ProgramacionDeFacturacion: {name: 'Programación de facturación', icon: 'fa-solid fa-gears'},
+    Rutas: {name: 'Rutas', icon: 'fa-solid fa-signs-post', url: ''},
+    TiposDeRutas : {name: 'Tipos de rutas', icon: 'fa-solid fa-sitemap', url: ''},
+    CiclosDeFacturacion: {name: 'Ciclos de facturación', icon: 'fa-solid fa-arrows-spin', url: ''},
+    ProgramacionDeFacturacion: {name: 'Programación de facturación', icon: 'fa-solid fa-gears', url: ''},
   }
 
 

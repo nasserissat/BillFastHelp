09108e5c364924770a17e11162feb562ae7550ab
Nasser Emil Issa Tavares <nissa@transnegrd.com>
2022-09-16 09:26:40 -0400

    Sidebar design completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchbarComponent } from './Components/searchbar/searchbar.component';
 import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { PagesComponent } from './pages/pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchbarComponent,
-    SideNavComponent
+    SideNavComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,

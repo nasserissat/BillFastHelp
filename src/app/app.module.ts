1b753b38547bf156e5a488797e4a33aaeb32375a
Nasser Emil Issa Tavares <nissa@transnegrd.com>
2022-09-16 11:09:25 -0400

    Side nav refactorizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchbarComponent } from './Components/searchbar/searchbar.component';
+import { ObjToArrayPipe } from './pipes/objToArray.pipe';
+import { SharedModule } from './shared/shared.module';
 import { SideNavComponent } from './shared/side-nav/side-nav.component';
-import { PagesComponent } from './pages/pages/pages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchbarComponent,
     SideNavComponent,
-    PagesComponent
+    ObjToArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  Transacciones = {
+    Facturas: {name:'Facturas', icon: 'fa-solid fa-file-invoice ml-0.5'},
+    Pagos: {name: 'Pagos', icon: 'fa-solid fa-receipt'},
+    CambioDeDivisas:{name: 'Cambios de divisas', icon: 'fa-solid fa-file-zipper'},
+    NotasDeCredito:{name: 'Notas de crédito', icon: 'fa-solid fa-money-check-dollar'},
+    NotasDeDebito:{name: 'Notas de débito', icon: 'fa-solid fa-file-invoice-dollar'},
+    Cargos:{name: 'Cargos', icon: 'fa-solid fa-file-invoice-dollar'},
+    LotesDeFacturacion:{name: 'Lotes de facturación', icon: 'fa-solid fa-file-zipper'},
+    ImpresionMasiva:{name: 'Impresión masiva', icon: 'fa-solid fa-file-zipper'},
+  }
+
+  Catalogos = {
+    Clientes: {name:'Clientes', icon: 'fa-solid fa-id-badge'},
+    Direcciones: {name: 'Direcciones', icon: 'fa-solid fa-signs-post'},
+    Suscripciones:{name: 'Suscripciones', icon: 'fa-solid fa-scroll'},
+    Productos:{name: 'Productos', icon: 'fa-solid fa-boxes-stacked'},
+    Materiales:{name: 'Materiales', icon: 'fa-solid fa-boxes-stacked'},
+  }
+
+  ServicioAlCliente = {
+    Tickets: {name:'Tickets', icon: 'fa-solid fa-ticket'},
+    Reclamaciones: {name: 'Reclamaciones', icon: 'fa-solid fa-id-badge'},
+    AcuerdosDePago:{name: 'Acuerdos de Pago', icon: 'fa-solid fa-file-signature'},
+    CertificadosDeNoAdeudo:{name: 'Certificados de No Adeudo', icon: 'fa-solid fa-stamp'},
+  }
+
+  GestionDeTrabajo = {
+    OrdenesDeTrabajo: {name: 'Ordenes de Trabajo', icon: 'fa-solid fa-folder'},
+    TiposDeOrdenesDeTrabajo: {name: 'Tipos de Ordenes de Trabajo', icon: 'fa-solid fa-folder-tree'},
+  }
+  Planificacion = {
+    Rutas: {name: 'Rutas', icon: 'fa-solid fa-signs-post'},
+    TiposDeRutas : {name: 'Tipos de rutas', icon: 'fa-solid fa-sitemap'},
+    CiclosDeFacturacion: {name: 'Ciclos de facturación', icon: 'fa-solid fa-arrows-spin'},
+    ProgramacionDeFacturacion: {name: 'Programación de facturación', icon: 'fa-solid fa-gears'},
+  }
+
+
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
+ 
 
 }

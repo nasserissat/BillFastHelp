c514f274448282291b26b85c2d7a692eac8350d1
Nasser <nasseremil25@gmail.com>
2022-09-17 12:57:25 -0400

    open, close, and click outside to close dropdown's header works

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  host: {
+    '(document:click)': 'onClickOutside($event)',
+  },
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild("AsDropdown") Dropdown!: ElementRef;
+  isLanguageMenuOpened: boolean = false;
+  isUserMenuOpened: boolean = false;
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
   }
+
+  toggleUserMenu(): void{
+    if(this.isLanguageMenuOpened == true){
+      this.isLanguageMenuOpened = !this.isLanguageMenuOpened;
+    }
+    this.isUserMenuOpened = !this.isUserMenuOpened;
+    
+  }
+  toogleLanguageMenu():void{
+    if(this.isUserMenuOpened == true){
+      this.isUserMenuOpened = !this.isUserMenuOpened;
+    }
+    this.isLanguageMenuOpened = !this.isLanguageMenuOpened;
+   
+  }
+  onClickOutside(event: { target: any; }){
+    const AsDropdown = this.Dropdown.nativeElement;
+    if(!this._eref.nativeElement.contains(event.target))
+    AsDropdown.classList.add('hidden');
+  }
+
 
 }
